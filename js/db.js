997a3734fea4e594c266093d368580fdad9b9323
harris112 <32365526+harris112@users.noreply.github.com>
2021-07-04 08:54:35 +0000

    add react

--- js/db.js
+++ js/db.js
@@ -1,15 +1,15 @@
-//Defining the data base
-var db = new Dexie("item_database");
-db.version(1).stores({
-  alerts: 'name, description, contact_details, latitude, longitude'
-});
+// //Defining the data base
+// var db = new Dexie("item_database");
+// db.version(1).stores({
+//   alerts: 'name, description, contact, latitude, longitude'
+// });
 
-//clear the tables when page loads
-db.version(2).stores({
-  alerts: 'name, description, contact_details, latitude, longitude'
-});
+// //clear the tables when page loads
+// db.version(2).stores({
+//   alerts: 'name, description, contact, latitude, longitude'
+// });
 
-function clearDB() {
-  db.alerts.clear();
-  location.reload();
-}
+// function clearDB() {
+//   db.alerts.clear();
+//   location.reload();
+// }
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://dailytimes.com.pk/assets/uploads/2017/10/24/Crime-scene-1280x720.jpg" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code>!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
