091ce4de6558346a48ee76ca1029de7118730b4c
Harris Masood <harrismasood15@gmail.com>
2021-07-06 17:42:35 +0500

    Updates

---
+++ precache-manifest.7da359a01f1c8050e6c223a49a54bd84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07bbaea81de31e21e78bca76fecc10c3",
+    "url": "/gamified-crime-alert/index.html"
+  },
+  {
+    "revision": "a78182f8fc54b7637f1e",
+    "url": "/gamified-crime-alert/static/css/main.c6367557.chunk.css"
+  },
+  {
+    "revision": "988fade28c68a1013234",
+    "url": "/gamified-crime-alert/static/js/2.00a428cd.chunk.js"
+  },
+  {
+    "revision": "a78182f8fc54b7637f1e",
+    "url": "/gamified-crime-alert/static/js/main.46034491.chunk.js"
+  },
+  {
+    "revision": "53c62a9b6e5e382520e9",
+    "url": "/gamified-crime-alert/static/js/runtime-main.14571150.js"
+  }
+]);
